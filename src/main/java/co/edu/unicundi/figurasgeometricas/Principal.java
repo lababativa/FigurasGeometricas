b3b9714e08a3f740799928c46b1bb3d28e68d8e5
lababativa <laubabativa.11@gmail.com>
2021-03-22 12:04:03 -0500

    Implementacion del menu en la clase principal

--- src/main/java/co/edu/unicundi/figurasgeometricas/Principal.java
+++ src/main/java/co/edu/unicundi/figurasgeometricas/Principal.java
@@ -5,29 +5,105 @@
  */
 package co.edu.unicundi.figurasgeometricas;
 
+import java.util.Scanner;
+
 /**
  * Clase Principal
  * @author LAURA BABATIVA
  */
 public class Principal {
     public static void main(String[] args) {
+        Scanner numeroOpc = new Scanner(System.in);
+        Scanner valor1 = new Scanner(System.in);
+        Scanner valor2 = new Scanner(System.in);
+        Scanner valor3 = new Scanner(System.in);
+        //Indica si salimos o no
+        boolean salir = false;
+        int opcion;
+        double radio;
+        double lado;
+        double lado2;
+        double lado3;
+        double base;
+        double altura;
         FigurasGeometricas figura;
-        figura = new Cuadrado(5);
-        System.out.println("Figura: "+figura.getNombre());
-        System.out.println("Area: "+figura.getArea());
-        System.out.println("Perimetro: "+figura.getPerimetro());
-        figura = new Circulo(5);
-        System.out.println("\nFigura: "+figura.getNombre());
-        System.out.println("Area: "+figura.getArea());
-        System.out.println("Perimetro: "+figura.getPerimetro());
-        figura = new Triangulo(8,5,5);
-        System.out.println("\nFigura: "+figura.getNombre());
-        System.out.println("Area: "+figura.getArea());
-        System.out.println("Perimetro: "+figura.getPerimetro());
-        figura = new Rectangulo(8,5);
-        System.out.println("\nFigura: "+figura.getNombre());
-        System.out.println("Area: "+figura.getArea());
-        System.out.println("Perimetro: "+figura.getPerimetro());
+        //Bucle para pedir las opciones hasta que elijamos salir
+        while (!salir) {
+
+            //opciones
+            System.out.println("---------Menu Principal---------");
+            System.out.println("----Escoge una opcion\n----Para hallar el perimetro y el area.");
+            System.out.println("1. Cuadrado\n2. Rectangulo\n3. Triangulo\n4. Circulon\n5. Salir");
+
+            try{
+            
+                //Pide una opcion
+                System.out.println("Introduce un numero: ");
+                opcion = numeroOpc.nextInt();
+
+                //Realiza una de las opciones
+                switch (opcion) {
+                    case 1:
+                        System.out.println("-AREA Y PERIMETRO DEL CUADRADO");
+                        System.out.println("Por favor ingresar el valor del lado: ");
+                        lado = valor1.nextInt();
+                        figura = new Cuadrado(lado);
+                        System.out.println("Figura: "+figura.getNombre());
+                        System.out.println("Area: "+figura.getArea());
+                        System.out.println("Perimetro: "+figura.getPerimetro());
+                        break;
+                    case 2:
+                        System.out.println("-AREA Y PERIMETRO DEL RECTANGULO");
+                        System.out.println("Por favor ingresar el valor de la base: ");
+                        base = valor1.nextInt();
+                        System.out.println("Por favor ingresar el valor de la altura: ");
+                        altura = valor2.nextInt();
+                        figura = new Rectangulo(base,altura);
+                        System.out.println("\nFigura: "+figura.getNombre());
+                        System.out.println("Area: "+figura.getArea());
+                        System.out.println("Perimetro: "+figura.getPerimetro());
+                        break;
+                    case 3:
+                        System.out.println("-AREA Y PERIMETRO DEL TRIANGULO");
+                        System.out.println("Por favor ingresar el valor del lado 1: ");
+                        lado = valor1.nextInt();
+                        System.out.println("Por favor ingresar el valor del lado 2: ");
+                        lado2 = valor2.nextInt();
+                        System.out.println("Por favor ingresar el valor del lado 3: ");
+                        lado3 = valor3.nextInt();
+                        figura = new Triangulo(lado,lado2,lado3);
+                        System.out.println("\nFigura: "+figura.getNombre());
+                        System.out.println("Area: "+figura.getArea());
+                        System.out.println("Perimetro: "+figura.getPerimetro());
+                        break;
+                    case 4:
+                        System.out.println("-AREA Y PERIMETRO DEL CIRCULO");
+                        System.out.println("Por favor ingresar el valor del radio: ");
+                        radio = valor1.nextInt();
+                        figura = new Circulo(radio);
+                        System.out.println("\nFigura: "+figura.getNombre());
+                        System.out.println("Area: "+figura.getArea());
+                        System.out.println("Perimetro: "+figura.getPerimetro());
+                        break;
+                    case 5:
+                        salir=true;
+                        break;
+                    default:
+                        System.out.println("La opcion no es valida\nPor favor introducir una opcion entre 1 y 5");
+                }
+            
+                
+            //controla la excepcionn en caso de que se introduzca un valor no correcto
+            }catch(Exception e){
+                System.out.println("Debes escribir un numero");
+                numeroOpc.next();
+            }
+
+        }
+        
+        System.out.println("Vuelve Pronto\nFin");
+        
+        
     }
 }
 
